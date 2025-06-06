29619b389731a0ba12cdaca12ce064ff6ff4d07e
Karima002 <karima.mouadi@hva.nl>
2025-06-06 11:28:02 +0200

    #7 Eerste opzet server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express' //impoteer express en liquid package als dependenct via npm
+
+import { Liquid } from 'liquidjs'
+
+const app = express()
+
+app.use(express.urlencoded({extended: true})) //maakt het werken met data uit formulieren prettiger
+
+app.use(express.static('public')) //voor de public map voor statische bestanden zoals CSS, Javascript
+
+const engine = new Liquid() // Liquid als view-engine instellen
+app.engine('liquid', engine.express())
+
+app.set('views', './views') //Instellen van de map met de Liquid templates
+
